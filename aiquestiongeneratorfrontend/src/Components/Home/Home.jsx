7ade15be4fbb4b15133f37cba6676d8c26f7081f
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-05-19 22:27:43 +0530

    minor updatation in home page and tailwind css installed

--- aiquestiongeneratorfrontend/src/Components/Home/Home.jsx
+++ aiquestiongeneratorfrontend/src/Components/Home/Home.jsx
@@ -1,7 +1,18 @@
 const Home = ()=>{
     return (
         <>
-            <div>Working fine..</div>
+            <div>
+                <div className="text-2xl text-red-600">
+                    <p>Welcome to AiQuestionGenerator</p>
+                    <p>What kind of question do you want to create!</p>
+                </div>
+                <div>
+                    <button>MCQ's</button>
+                    <button>Subjective</button>
+                    <button>True/False</button>
+                </div>
+
+            </div>
         </>
     )
 }
