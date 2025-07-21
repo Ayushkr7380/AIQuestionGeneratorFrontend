58587d9fc5ba38c14abaa3422a103678da5b853d
ayush kumar <ayushkumar19999@gmail.com>
2025-07-21 23:59:57 +0530

    toastify updated

--- aiquestiongeneratorfrontend/src/utils/Toastify.jsx
+++ aiquestiongeneratorfrontend/src/utils/Toastify.jsx
@@ -1,7 +1,16 @@
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const Toastify = () => <ToastContainer position="top-center" />;
+const Toastify = () => <ToastContainer
+    position="top-center"
+    autoClose={3000} // ⏱️ Toast will auto close in 3 seconds
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    pauseOnHover
+    draggable
+    pauseOnFocusLoss
+  />;
 
 export const showErrorToast = (msg) => toast.error(msg);
 export const showSuccessToast = (msg) => toast.success(msg);
